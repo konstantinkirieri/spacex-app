85cf29ff62b681945476dae27831dc408075ce52
Anton Liverov <liverov@yandex.ru>
2021-08-01 18:05:44 +0300

    Delete table component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,9 @@
 import S from './styles.module.css'
-import Table from '../Table'
 
 export default function App() {
   return (
     <div className={S.app}>
-      <span className={S.todos}>todos</span>
-      <Table />
-      <span>Double-click to edit a todo</span>
+        <h1>Start</h1>
     </div>
   )
 }
