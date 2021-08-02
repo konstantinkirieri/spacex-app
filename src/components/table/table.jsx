a7672c7b9bff825d218925a4a6d20c4f6ad0e701
Anton Liverov <liverov@yandex.ru>
2021-08-02 21:07:27 +0300

    Added Description component

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,13 +1,14 @@
 import S from "./styles.module.css"
 import {Categories} from "../category/category";
 import List from "../list/list";
+import {Description} from "../Description";
 
 export default function Table() {
     return(
         <main className={S.main}>
             <Categories />
             <List />
-            <div>Description</div>
+            <Description />
         </main>
     )
 }
