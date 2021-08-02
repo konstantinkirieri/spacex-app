f2a7dc7e087e765537e220a3338976b504a212c5
Ilia <iliapanin300999@mail.ru>
2021-08-02 18:58:31 +0300

    Данные в monks
    Перенес данные из компоненты "Categories" в monks/categories

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -2,17 +2,10 @@ import React from 'react'
 import S from './categories.module.css'
 import fav from '../../img/heart.svg'
 import { CategoryItem } from './CategoryItem';
+import { categories } from '../../mocks/categories';
 
 
 export function Categories() {
-
-   const categories = [
-      { id: 1, name: "Capsules", endpoint: "./capsules" },
-      { id: 2, name: "Rockets", endpoint: "./rockets" },
-      { id: 3, name: "Ships", endpoint: "./ships" },
-      { id: 4, name: "Launches", endpoint: "./launches" },
-   ]
-
 
    const allCategories = categories.map((item, i) => {
       return <CategoryItem key={item.id} data={item} index={i + 1} />
---
+++ src/mocks/categories.js
@@ -0,0 +1,6 @@
+export const categories = [
+   { id: 1, name: "Capsules", endpoint: "./capsules" },
+   { id: 2, name: "Rockets", endpoint: "./rockets" },
+   { id: 3, name: "Ships", endpoint: "./ships" },
+   { id: 4, name: "Launches", endpoint: "./launches" },
+]
