16edae5917e43fa6afb5adb99759f8b0554b2942
Anton Liverov <liverov@yandex.ru>
2021-08-02 21:00:31 +0300

    Update index.jsx

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,17 +1,13 @@
-import Header from '../header/header'
-import {Description} from '../Description'
+import Header from "../header/header";
+import Table from "../table/table";
 
 import S from './styles.module.css'
 
 export default function App() {
   return (
     <div className={S.app}>
-      <Header />
-      <main className={S.main}>
-        <div className={S.categories}>Categories</div>
-        <div className={S.table}>Table</div>
-        <Description />
-      </main>
+        <Header />
+        <Table />
     </div>
   )
 }
