3b50b0f42f874a984221c10bb46ed6ca59293c98
SashaLisitsyn <sasha.lisitsyn.work@yandex.ru>
2021-08-02 18:56:39 +0300

    Сверстал компонент Description. Пропсы пока не стал добавлять

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,15 +1,17 @@
-import Header from "../header/header";
+import Header from '../header/header'
+import {Description} from '../Description'
 
 import S from './styles.module.css'
 
 export default function App() {
   return (
     <div className={S.app}>
-        <Header />
-        <main className={S.main}>
-            <div className={S.categories}>Categories</div>
-            <div className={S.table}>Table</div>
-        </main>
+      <Header />
+      <main className={S.main}>
+        <div className={S.categories}>Categories</div>
+        <div className={S.table}>Table</div>
+        <Description />
+      </main>
     </div>
   )
 }
