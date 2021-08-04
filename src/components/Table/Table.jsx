8d078f0eb5baab2e95f169e9a9030b216225c257
Anton Liverov <liverov@yandex.ru>
2021-08-04 12:31:53 +0300

    Add category state

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,12 +2,19 @@ import S from "./styles.module.css"
 import {Categories} from "../Category/Category";
 import List from "../List/List";
 import {Description} from "../Description/Description";
+import {useState} from "react";
 
 export default function Table() {
+    const [category, setCategory] = useState(null);
+
+    const handlerChangeCategory = (name) => {
+        setCategory(name);
+    }
+
     return(
         <main className={S.main}>
-            <Categories />
-            <List />
+            <Categories onChangeCategory={handlerChangeCategory}/>
+            <List category={category}/>
             <Description />
         </main>
     )
