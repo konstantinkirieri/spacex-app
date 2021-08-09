59cd1f269ac6f0e812fcc085c5e911cfab13f17a
konstantinkirieri <kirieris@mail.ru>
2021-08-09 13:55:22 +0300

    Remove success from launch and add pointer on like

--- src/components/ListItemLaunches/ListItemLaunches.jsx
+++ src/components/ListItemLaunches/ListItemLaunches.jsx
@@ -9,7 +9,6 @@ export default function ListItemLaunches ({onChangeItem, launches}) {
         title={item.name}
         urlImg={item.links.patch.small}
         description={item.details}
-        success={item.success}
         favorite={item.isFavorite}
         onChangeItem={() => onChangeItem(item.id)}
       />)
