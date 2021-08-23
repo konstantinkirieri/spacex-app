79ccb32f28efc2953f24cbf03a01bebbcfe0c1d4
Anton Liverov <liverov@yandex.ru>
2021-08-23 22:39:06 +0300

    Modify theme

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import S from './styles.module.css'
 import {categories} from './categories'
 import {mainStore} from '../../stores'
+import {observer} from 'mobx-react'
 
-export const Categories: React.FC = () => {
+export const Categories: React.FC = observer(() => {
   return (
     <nav className={S.category_wrapper}>
       {categories.map((item: {name: string; id: number}) => {
         return (
           <div
             key={item.id}
-            className={S.item}
+            className={`${S.item} ${
+              mainStore.currentCategory === item.name && S.item_active
+            }`}
             onClick={() => (mainStore.changeCategory = item.name)}>
             {item.name}
           </div>
@@ -18,4 +21,4 @@ export const Categories: React.FC = () => {
       })}
     </nav>
   )
-}
+})
