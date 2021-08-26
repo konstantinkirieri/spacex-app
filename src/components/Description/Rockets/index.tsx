6e2efb87cd800c6645a3c48b99a006375569d7c1
Anton Liverov <liverov@yandex.ru>
2021-08-26 17:32:46 +0300

    mini fix rockets info

--- src/components/Description/Rockets/index.tsx
+++ src/components/Description/Rockets/index.tsx
@@ -2,18 +2,29 @@ import React from 'react'
 import {IRocketsData} from '../../../interfaces'
 
 import S from '../styles.module.css'
+import {mainStore} from '../../../stores'
 
-export const DescriptionRockets: React.FC<{rocketsItem: IRocketsData}> = ({
-  rocketsItem
-}) => {
-  const {name, height, diameter, mass, flickr_images} = rocketsItem
+export const DescriptionRockets: React.FC<{
+  rocketsItem: IRocketsData
+}> = ({rocketsItem}) => {
+  const {name, height, diameter, mass, flickr_images} =
+    rocketsItem
   return (
     <div className={S.rocketList__info}>
-      <div>
-        <img className={S.rocketList__image} src={flickr_images[0]} alt={name} />
-      </div>
+      {mainStore.currentCategory !== 'Rockets' && (
+        <div>
+          <img
+            className={S.rocketList__image}
+            src={flickr_images[0]}
+            alt={name}
+          />
+        </div>
+      )}
+
       <div className={S.rocketList__details}>
-        <h3>{name}</h3>
+        <h3>
+          {mainStore.currentCategory !== 'Rockets' && name}
+        </h3>
         <div>Height: {height.meters} meters</div>
         <div>Diameter: {diameter.meters} meters</div>
         <div>Mass: {mass.kg} kg</div>
